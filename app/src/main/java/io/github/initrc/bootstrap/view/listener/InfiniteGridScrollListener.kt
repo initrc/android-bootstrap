7c22e93682fade73526ca215a8aa349d72d193b8
David Shi <DavidSnooker@gmail.com>
2018-02-15 18:28:33 -0800

    Let InfiniteGridScrollListener trigger another load if the column count changes

--- app/src/main/java/io/github/initrc/bootstrap/view/listener/InfiniteGridScrollListener.kt
+++ app/src/main/java/io/github/initrc/bootstrap/view/listener/InfiniteGridScrollListener.kt
@@ -7,19 +7,19 @@ import android.support.v7.widget.StaggeredGridLayoutManager
  * Infinite scroll listener.
  */
 class InfiniteGridScrollListener(
-        val layoutManager: StaggeredGridLayoutManager, val func: () -> Unit)
+        private val layoutManager: StaggeredGridLayoutManager, private val loadMore: () -> Unit)
         : RecyclerView.OnScrollListener() {
-    var prevTotalCount = 0
-    var totalCount = 0
-    var visibleCount = 0
-    val visibleThreshold = layoutManager.spanCount * 5
-    var loading = true
+    private var prevTotalCount = 0
+    private var totalCount = 0
+    private var visibleCount = 0
+    private var loading = true
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-        if (dy <= 0) return
+        if (dy < 0) return
         totalCount = layoutManager.itemCount
         visibleCount = layoutManager.childCount
+        val visibleThreshold = layoutManager.spanCount * 5
         val firstVisiblePos = IntArray(layoutManager.spanCount)
         layoutManager.findFirstVisibleItemPositions(firstVisiblePos)
 
@@ -31,7 +31,7 @@ class InfiniteGridScrollListener(
         }
         if (!loading && totalCount - visibleCount
                 <= firstVisiblePos.getOrElse(0) {0} + visibleThreshold) {
-            func()
+            loadMore()
             loading = true
         }
     }
